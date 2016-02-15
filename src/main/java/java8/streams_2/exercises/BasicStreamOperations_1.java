96bb8a29b1ec3197cf54a9e68e57286691814647
dorian <dorian@groundcontrol>
2016-02-15 23:22:07 +0100

    First solutions

--- src/main/java/java8/streams_2/exercises/BasicStreamOperations_1.java
+++ src/main/java/java8/streams_2/exercises/BasicStreamOperations_1.java
@@ -1,26 +1,33 @@
 package java8.streams_2.exercises;
 
-import java8.exercises_0.Exercises;
-import java8.testdata_0.Album;
-import java8.testdata_0.Artist;
+import static java.util.stream.Collectors.toList;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import java8.testdata_0.Album;
+import java8.testdata_0.Artist;
 
 public class BasicStreamOperations_1 {
     // function that adds up numbers, i.e., int addUp(Stream<Integer> numbers)
     public static int addUp(Stream<Integer> numbers) {
-        return Exercises.replaceThisWithSolution();
+        return numbers.mapToInt(s1->s1.intValue()).sum();
     }
 
     // function that takes in artists and returns a list of strings with their names and places of origin
     public static List<String> getNamesAndOrigins(List<Artist> artists) {
-        return Exercises.replaceThisWithSolution();
+    	return artists.stream()
+                .flatMap(artist -> Stream.of(artist.getName(), artist.getNationality()))
+                .collect(toList());
     }
 
     // function that takes in albums and returns a list of albums with at most three
     // tracks
     public static List<Album> getAlbumsWithAtMostThreeTracks(List<Album> input) {
-        return Exercises.replaceThisWithSolution();
+        return input //
+        		.stream() //
+        		.filter(a1->a1.getTracks().count()<4) //
+        		.collect(Collectors.toList());
     }
 }
